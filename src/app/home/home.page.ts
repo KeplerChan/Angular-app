9116f4fefba8645ad2e5f4ab6e86c960ef9a03d8
mychanaz <mychanaz@connect.ust.hk>
2023-05-26 00:02:18 +0800

    third version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,4 +12,5 @@ export class HomePage {
   gotoS3() {  
     this.router.navigate(['s3']);  
   }  /*https://www.javatpoint.com/ionic-navigation-and-routing*/
+
 }  
--- src/app/s3/s3.page.ts
+++ src/app/s3/s3.page.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class S3Page implements OnInit {
 
-  constructor() { }
+  /*constructor() { }*/
 
   ngOnInit() {
   }
 
+}
+
+export class runningRecord {
+  /* https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve */
+  gaussianRandom(mean=0, stdev=1) {
+    let u = 1 - Math.random(); // Converting [0,1) to (0,1]
+    let v = Math.random();
+    let z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    // Transform to the desired mean and standard deviation:
+    return z * stdev + mean;
+  }
+
+  runningRecord: RunningRecord = {
+    activityTime: Date.now();
+    distance: gaussianRandom(mean=8100, stdev=500);/* in unit of meters*/
+    duration: gaussianRandom(mean=42*60+16, stdev=5*60);
+    pacing: duration/distance;
+    stride:  gaussianRandom(mean=1.1, stdev=0.5)
+    steps: Math.floor(distance/Stride);
+    calories: 723;
+    cadence: steps/(duration/60);
+
+  }
+}
+
+
+export class RunningRecord {
+
+  activityTime: Date;
+  distance:     number;
+  duration:      number;
+  pacing:       number;
+  duration:     number;
+  stride         number;
+  steps:        number;
+  calories:     number;
+  cadence       number;
+  
 }
