fd083a570425aa3792aab62ebc1d083cf9ee3191
mychanaz <mychanaz@connect.ust.hk>
2023-05-26 22:00:54 +0800

    made running record for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,12 +32,11 @@ export class AppComponent {
       this.ob = this.ob.concat(a)
       }
     }
-    gaussianRandom(mean=0, stdev=1) {
+  gaussianRandom(mean=0, stdev=1) {
       let u = 1 - Math.random(); // Converting [0,1) to (0,1]
       let v = Math.random();
       let z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-// Transform to the desired mean and standard deviation:
       return z * stdev + mean;
-    }
+  }
 
 }
--- src/app/s3/s3.page.ts
+++ src/app/s3/s3.page.ts
@@ -2,16 +2,42 @@ import { Component, OnInit, Input} from '@angular/core';
 import { Router } from '@angular/router';  
 
 @Component({
-  selector: 'apps3',
+  selector: 's3',
   templateUrl: './s3.page.html',
   styleUrls: ['./s3.page.scss'],
 })
 
 export class S3Page implements OnInit {
-
-  constructor(private router: Router){
-
+  ob:Array<{
+    activityTime: number,
+    distance: number,
+      pacing: number,
+    duration: number,
+      stride: number,
+       steps: number,
+    calories: number,
+     cadence: number}>;
+  constructor(private router: Router) {
+    console.log("hi")
+    this.ob = Array(0).fill(null);
+    for (let i = 0; i < 10000; i++) {
+      let a = {
+      activityTime: i,
+      distance:        1,
+      duration:      1,
+      pacing:        1,
+      stride:     1,
+      steps:          1,
+      calories:        1,
+      cadence:     1 }
+      this.ob = this.ob.concat(a)
+      }
   }
+
+  
+
+
+
   ngOnInit() {
   }
   goBack() {  
