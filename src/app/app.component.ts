c8001e1b36ff92b191e5df5ed913422815a5eedd
mychanaz <mychanaz@connect.ust.hk>
2023-05-26 21:49:20 +0800

    complete generating random running records

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,31 +9,35 @@ import { S3Page } from './s3/s3.page';
 export class AppComponent {
 
   ob:Array<{activityTime: number,
-    distance:      number,
-    pacing:       number,
-    duration:     number,
-    stride:         number,
-    steps:        number,
-    calories:     number,
-    cadence:       number}>;
+    distance: number,
+      pacing: number,
+    duration: number,
+      stride: number,
+       steps: number,
+    calories: number,
+     cadence: number}>;
   constructor() {
-    this.ob = Array(10000).fill({
-      activityTime:   1,
-      distance:       this.gaussianRandom(8100, 500),
-      duration:       this.gaussianRandom(42*60+16,5*60),
-      pacing:         1,
-      stride:         this.gaussianRandom(1.1,0.5),
+    console.log("hi")
+    this.ob = Array(0).fill(null);
+    for (let i = 0; i < 10000; i++) {
+      let a = {
+      activityTime: i,
+      distance:        this.gaussianRandom(8100, 500),
+      duration:      this.gaussianRandom(42*60+16,5*60),
+      pacing:        1,
+      stride:      this.gaussianRandom(1.1,0.5),
       steps:          1,
-      calories:       723,
-      cadence:        1
-    });
-  }
-  gaussianRandom(mean=0, stdev=1) {
-    let u = 1 - Math.random(); // Converting [0,1) to (0,1]
-    let v = Math.random();
-    let z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-    // Transform to the desired mean and standard deviation:
-    return z * stdev + mean;
-  }
+      calories:        723,
+      cadence:     1 }
+      this.ob = this.ob.concat(a)
+      }
+    }
+    gaussianRandom(mean=0, stdev=1) {
+      let u = 1 - Math.random(); // Converting [0,1) to (0,1]
+      let v = Math.random();
+      let z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+// Transform to the desired mean and standard deviation:
+      return z * stdev + mean;
+    }
 
 }
--- src/app/s3/s3.page.ts
+++ src/app/s3/s3.page.ts
@@ -9,37 +9,8 @@ import { Router } from '@angular/router';
 
 export class S3Page implements OnInit {
 
-  ob:Array<{activityTime: number,
-                distance: number,
-                  pacing: number,
-                duration: number,
-                  stride: number,
-                   steps: number,
-                calories: number,
-                 cadence: number}>;
-  constructor(private router: Router) {
-    console.log("hi")
-    this.ob = Array(0).fill(null);
-    for (let i = 0; i < 10000; i++) {
-      let a = {
-        activityTime: i,
-        distance:        this.gaussianRandom(8100, 500),
-        duration:      this.gaussianRandom(42*60+16,5*60),
-        pacing:        1,
-        stride:      this.gaussianRandom(1.1,0.5),
-        steps:          1,
-        calories:        723,
-        cadence:     1 }
-      this.ob = this.ob.concat(a)
+  constructor(private router: Router){
 
-    }
-  }
-  gaussianRandom(mean=0, stdev=1) {
-    let u = 1 - Math.random(); // Converting [0,1) to (0,1]
-    let v = Math.random();
-    let z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-    // Transform to the desired mean and standard deviation:
-    return z * stdev + mean;
   }
   ngOnInit() {
   }
