c2f5b9daa1892d38406e2bd6699c61091fb87c77
mychanaz <mychanaz@connect.ust.hk>
2023-05-27 12:52:02 +0800

    completed completing buttons in s3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ import { S3Page } from './s3/s3.page';
 export class AppComponent {
 
   ob:Array<{
-    activityTime: number,
+    activitydate: string,
+    activitytime: string,
     distance: number,
       pacing: number,
     duration: number,
@@ -20,21 +21,31 @@ export class AppComponent {
   constructor() {
     console.log("hi")
     this.ob = Array(0).fill(null);
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {    /*adjust the number of records here*/ 
       let dist = this.gaussianRandom(8100, 500), dura = this.gaussianRandom(42*60+16,5*60), stri = this.gaussianRandom(1.1,0.5);
       let nextrecord = {
-      activityTime: i,
-      distance:        dist/1000, /* unit:km*/
-      duration:      dura/60, /* unit:min*/
-      pacing:       dura/60 /dist*1000, /* unit:min per km*/
+      activitydate: this.formatDate(),
+      activitytime: "9:00am",
+      distance:        dist, /* unit:meter*/
+      duration:      dura, /* unit: second*/
+      pacing:       dura/dist, /* unit:second per meter*/
       stride:     stri, /* unit:meter*/
       steps:          dist/stri, /* unit: null*/
       calories:        723, /* kcal*/
-      cadence:     dist/stri/dura*60 /* unit:steps  per min*/
+      cadence:     dist/stri/dura /* unit:steps  per second*/
       } 
       this.ob = this.ob.concat(nextrecord)
     }
   }
+
+  formatDate(date = new Date()) {
+    const year = date.toLocaleString('en-GB', {year: 'numeric'});
+    const month = date.toLocaleString('en-GB', {month: '2-digit'});
+    const day = date.toLocaleString('en-GB', {day: '2-digit'});
+  
+    return [year, month, day].join('/');
+  }
+
   gaussianRandom(mean=0, stdev=1) {
     let u = 1 - Math.random(); // Converting [0,1) to (0,1]
     let v = Math.random();
--- src/app/s3/s3.page.ts
+++ src/app/s3/s3.page.ts
@@ -9,7 +9,8 @@ import { Router } from '@angular/router';
 
 export class S3Page implements OnInit {
   ob:Array<{
-    activityTime: number,
+    activitydate: string,
+    activitytime: string,
     distance: number,
       pacing: number,
     duration: number,
@@ -20,21 +21,39 @@ export class S3Page implements OnInit {
   constructor(private router: Router) {
     console.log("hi")
     this.ob = Array(0).fill(null);
-    for (let i = 0; i < 10; i++) {
-      let dist = this.gaussianRandom(8100, 500), dura = this.gaussianRandom(42*60+16,5*60), stri = this.gaussianRandom(1.1,0.5);
-      let nextrecord = {
-      activityTime: i,
-      distance:        dist/1000, /* unit:km*/
-      duration:      dura/60, /* unit:min*/
-      pacing:       dura/60 /dist*1000, /* unit:min per km*/
+    for (let i = 0; i < 10; i++) {    /*adjust the number of records here*/ 
+      let dist = this.gaussianRandom(8100, 500), 
+      dura = this.gaussianRandom(42*60+16,5*60), 
+      stri = this.gaussianRandom(1.1,0.5),
+      now = new Date();
+      let
+      hour = now.toLocaleString('en-GB', {hour: '2-digit'}),
+      second = now.toLocaleString('en-GB', {second: '2-digit'}),
+      nextrecord = {
+      activitydate: this.formatDate(),
+      activitytime: [hour,second].join(":"),
+      distance:        dist, /* unit:meter*/
+      duration:      dura, /* unit: second*/
+      pacing:       dura/dist, /* unit:second per meter*/
       stride:     stri, /* unit:meter*/
       steps:          dist/stri, /* unit: null*/
       calories:        723, /* kcal*/
-      cadence:     dist/stri/dura*60 /* unit:steps  per min*/
+      cadence:     dist/stri/dura /* unit:steps  per second*/
       } 
       this.ob = this.ob.concat(nextrecord)
     }
   }
+
+  formatDate(date = new Date()) {
+    const year = date.toLocaleString('en-GB', {year: 'numeric'});
+    const month = date.toLocaleString('en-GB', {month: '2-digit'});
+    const day = date.toLocaleString('en-GB', {day: '2-digit'});
+  
+    return [year, month, day].join('/');
+  }
+
+
+
   gaussianRandom(mean=0, stdev=1) {
     let u = 1 - Math.random(); // Converting [0,1) to (0,1]
     let v = Math.random();
